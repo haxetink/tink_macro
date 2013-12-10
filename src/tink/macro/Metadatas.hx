037f15929fe317baaf6ed55fe61f4136599527c8
back2dos <back2dos@gmail.com>
2013-12-10 20:47:42 +0100

    Nullcheck + cleanup.

--- src/tink/macro/Metadatas.hx
+++ src/tink/macro/Metadatas.hx
@@ -5,16 +5,17 @@ import haxe.macro.Expr;
 class Metadatas {	
 	static public function toMap(m:Metadata) {
 		var ret = new Map<String,Array<Array<Expr>>>();
-		for (meta in m) {
-			if (!ret.exists(meta.name))
-				ret.set(meta.name, []);
-			ret.get(meta.name).push(meta.params);
-		}
+		if (m != null)
+			for (meta in m) {
+				if (!ret.exists(meta.name))
+					ret.set(meta.name, []);
+				ret.get(meta.name).push(meta.params);
+			}
 		return ret;
 	}
 	
 	static public function getValues(m:Metadata, name:String)
-		return [for (meta in m) 
-			if (meta.name == name) meta.params
-		];	
+		return 
+			if (m == null) [];
+			else [for (meta in m) if (meta.name == name) meta.params];	
 }
