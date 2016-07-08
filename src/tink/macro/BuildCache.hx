f2b670f9e4e4eb3e731ff592602fb6ee5158823d
Juraj Kirchheim <back2dos@googlemail.com>
2016-07-08 11:48:40 +0200

    Expand build cache for 2 and 3 type parameters.

--- src/tink/macro/BuildCache.hx
+++ src/tink/macro/BuildCache.hx
@@ -5,11 +5,22 @@ import haxe.macro.Expr;
 import haxe.macro.Type;
 import tink.macro.TypeMap;
 
+using haxe.macro.ComplexTypeTools;
+using haxe.macro.TypeTools;
+
 typedef BuildContext = {
   pos:Position,
   type:Type,
   usings:Array<TypePath>,
   name:String,
+}
+
+typedef BuildContext2 = {>BuildContext,
+  type2:Type,
+}
+
+typedef BuildContext3 = {>BuildContext2,
+  type3:Type,
 }
 
 class BuildCache { 
@@ -27,6 +38,50 @@ class BuildCache {
     refresh();
     
     return cache;
+  }
+  
+  static public function getType3(name, ?types, ?pos:Position, build:BuildContext3->TypeDefinition) {
+     if (types == null)
+      switch Context.getLocalType() {
+        case TInst(_.toString() == name => true, [t1, t2, t3]):
+          types = { t1: t1, t2: t2, t3: t3 };
+        default:
+          throw 'assert';
+      }  
+      
+    var t1 = types.t1.toComplexType(),
+        t2 = types.t2.toComplexType(),
+        t3 = types.t2.toComplexType();
+        
+    return getType(name, (macro : { t1: $t1, t2: $t2, t3: $t3 } ).toType(), pos, function (ctx) return build({
+      type: types.t1,
+      type2: types.t2,
+      type3: types.t3,
+      pos: ctx.pos,
+      name: ctx.name,
+      usings: ctx.usings
+    }));   
+  }
+  
+  static public function getType2(name, ?types, ?pos:Position, build:BuildContext2->TypeDefinition) {
+    if (types == null)
+      switch Context.getLocalType() {
+        case TInst(_.toString() == name => true, [t1, t2]):
+          types = { t1: t1, t2: t2 };
+        default:
+          throw 'assert';
+      }  
+      
+    var t1 = types.t1.toComplexType(),
+        t2 = types.t2.toComplexType();
+        
+    return getType(name, (macro : { t1: $t1, t2: $t2 } ).toType(), pos, function (ctx) return build({
+      type: types.t1,
+      type2: types.t2,
+      pos: ctx.pos,
+      name: ctx.name,
+      usings: ctx.usings
+    }));
   }
   
   static public function getType(name, ?type, ?pos:Position, build:BuildContext->TypeDefinition) {
